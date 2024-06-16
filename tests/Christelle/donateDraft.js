aab6c47b758e41f00bd3d89a2f29e540526fb412
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-16 11:15:20 -0400

    Updated HTML, CSS, and JavaScript

--- tests/Christelle/donateDraft.js
+++ tests/Christelle/donateDraft.js
@@ -1,3 +1,21 @@
+var input = document.getElementById('donation-amount-custom');
+
+// Add event listener for input changes
+input.addEventListener('input', function() {
+    // Get the current value of the input field
+    var value = input.value;
+    
+    // Check if the value already starts with a dollar sign
+    if (!value.startsWith('$')) {
+        // Prepend a dollar sign to the input value
+        input.value = '$' + value;
+    }
+});
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const input = document.getElementById("dollarInput");
 
